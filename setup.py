8cfa61ae991170fa3695c5e0cea1c7e2f063098a
vishnuprasadh <vishnuprasadh@gmail.com>
2017-03-26 00:29:57 +0530

    Updated 6 space indent to 4 and removed zip_flag, added url, download url

--- setup.py
+++ setup.py
@@ -1,11 +1,13 @@
 from setuptools import setup
 
-setup(name="DIG", description='Dynamically generates runtime image file resized based on bandwidth, screensize',
-      author='vishnu prasad hari',
-      author_email='vishnuprasadh@gmail.com',
-      keywords='Pillow-SIMD,PIP,Dynamic image rendering,dynamicimage,Image rendering',
-      license='MIT',
-      py_modules=['imgprocessor'],
-      version='0.1',
-      install_requires=['Pillow-SIMD','numpy','configparser','setuptools'],
-      zip_flag=False)
+setup(name="imgprocessor",
+    description='Dynamically generates runtime image file resized based on bandwidth, screensize',
+    author='vishnu prasad hari',
+    author_email='vishnuprasadh@gmail.com',
+    keywords='Pillow-SIMD,PIP,Dynamic image rendering,dynamicimage,Image rendering',
+    license='MIT',
+    py_modules=['imgprocessor'],
+    version='0.1',
+    download_url='https://github.com/vishnuprasadh/imgprocessor.git',
+    url='https://github.com/vishnuprasadh/imgprocessor',
+    install_requires=['Pillow-SIMD','numpy','configparser','setuptools'])
